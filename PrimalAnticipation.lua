7767ffa828e6901328da60a1504881903b28a3c0
Lukas Waymann <meribold@gmail.com>
2014-06-22 23:57:25 +0000

    Update display on PLAYER_ENTERING_WORLD

--- PrimalAnticipation.lua
+++ PrimalAnticipation.lua
@@ -497,6 +497,7 @@ end
 function handlerFrame:PLAYER_ENTERING_WORLD()
   comboTargetGUID = nil
   comboPoints = 0
+  comboPointFrame:update()
 end
 
 ------------------------------------------------------------------------------------------------------------------------
